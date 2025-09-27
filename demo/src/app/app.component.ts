711a1c297d3a8bc19c6794892f99b9998b6a77ee
榊原昌彦 <sakakibara@rdlabo.jp>
2025-09-27 12:07:16 +0900

    feat(demo): move into demo

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import * as allIcons from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonRouterOutlet, IonApp],
+  templateUrl: './app.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    addIcons(allIcons);
+  }
 }
