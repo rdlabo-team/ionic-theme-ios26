ed7647c993122def062fe753495e8c87219c2bbe
榊原昌彦 <sakakibara@rdlabo.jp>
2025-09-28 18:54:09 +0900

    feat(action-sheet): implement

--- demo/src/app/index/pages/action-sheet/action-sheet.page.ts
+++ demo/src/app/index/pages/action-sheet/action-sheet.page.ts
@@ -1,17 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonBackButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonItemGroup,
+  IonLabel,
+  IonList,
+  IonText,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-action-sheet',
   templateUrl: './action-sheet.page.html',
   styleUrls: ['./action-sheet.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton],
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonButtons,
+    IonBackButton,
+    IonList,
+    IonItem,
+    IonItemGroup,
+    IonLabel,
+    IonIcon,
+    IonText,
+  ],
 })
 export class ActionSheetPage implements OnInit {
-  constructor() {}
+  readonly actionSheetCtrl = inject(ActionSheetController);
 
   ngOnInit() {}
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Actions',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: 'trash-outline',
+          data: {
+            action: 'delete',
+          },
+        },
+        {
+          text: 'Share',
+          icon: 'share-outline',
+          data: {
+            action: 'share',
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
+
+    await actionSheet.present();
+  }
 }
