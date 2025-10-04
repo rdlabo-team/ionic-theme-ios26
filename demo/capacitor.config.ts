152edc90801a023b7ae02d26ef39c1e041a3a5a8
榊原昌彦 <sakakibara@rdlabo.jp>
2025-10-04 18:29:02 +0900

    chore(): add capacitor ios for view check

--- demo/capacitor.config.ts
+++ demo/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'demo',
+  appId: 'io.ionic.theme.ios26',
+  appName: 'theme-ios26',
   webDir: 'www',
 };
 
