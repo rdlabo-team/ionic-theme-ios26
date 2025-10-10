de188798aa54e29c4f8cbf69b49421ab1dcaba93
榊原昌彦 <sakakibara@rdlabo.jp>
2025-10-10 15:14:26 +0900

    feat(docs): add docs tab for explain

--- demo/src/app/docs/docs-page.component.spec.ts
+++ demo/src/app/docs/docs-page.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HealthPage } from './health-page.component';
+import { HealthPage } from './docs-page.component';
 import { testConfig } from '../../../util/test.config';
 
 describe('HealthPage', () => {
---
+++ demo/src/app/docs/docs-page.component.ts
@@ -0,0 +1,47 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonItemGroup,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonNote,
+  IonText,
+  IonTitle,
+  IonToolbar,
+  Platform,
+} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-docs-page',
+  templateUrl: './docs-page.component.html',
+  styleUrls: ['./docs-page.component.scss'],
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonIcon,
+    IonButton,
+    IonButtons,
+    IonList,
+    IonListHeader,
+    IonLabel,
+    IonNote,
+    IonItemGroup,
+    IonItem,
+    IonText,
+  ],
+})
+export class DocsPage {
+  readonly platform = inject(Platform);
+}
