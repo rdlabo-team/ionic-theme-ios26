9a0ab2cd262414a5d73154658b939a06ab59bf64
榊原昌彦 <sakakibara@rdlabo.jp>
2025-10-11 19:47:14 +0900

    feat(gesture): add tab-button gesture

--- demo/src/app/tabs/tabs.page.ts
+++ demo/src/app/tabs/tabs.page.ts
@@ -1,7 +1,25 @@
 import { Component, ElementRef, inject, OnInit } from '@angular/core';
-import { IonIcon, IonLabel, IonTabBar, IonTabButton, IonTabs } from '@ionic/angular/standalone';
+import { IonIcon, IonLabel, IonTabBar, IonTabButton, IonTabs, ViewDidEnter } from '@ionic/angular/standalone';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
+import { createAnimation } from '@ionic/angular/standalone';
+import { createGesture, GestureDetail } from '@ionic/angular';
+import type { Animation } from '@ionic/core/dist/types/utils/animation/animation-interface';
+import { Gesture } from '@ionic/core/dist/types/utils/gesture';
+
+const cloneElement = (tagName: string): HTMLElement => {
+  const getCachedEl = document.querySelector(`${tagName}.ion-cloned-element`);
+  if (getCachedEl !== null) {
+    return getCachedEl as HTMLElement;
+  }
+
+  const clonedEl = document.createElement(tagName) as HTMLElement;
+  clonedEl.classList.add('ion-cloned-element');
+  clonedEl.style.setProperty('display', 'none');
+  document.body.appendChild(clonedEl);
+
+  return clonedEl;
+};
 
 @Component({
   selector: 'app-tabs',
@@ -9,7 +27,7 @@ import { filter } from 'rxjs';
   styleUrls: ['tabs.page.scss'],
   imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
 })
-export class TabsPage implements OnInit {
+export class TabsPage implements OnInit, ViewDidEnter {
   readonly #router = inject(Router);
   readonly #el = inject(ElementRef);
   ngOnInit() {
@@ -25,4 +43,129 @@ export class TabsPage implements OnInit {
       }
     });
   }
+
+  ionViewDidEnter() {
+    createTabBarGesture();
+  }
 }
+
+/**
+ * Experimental
+ */
+const createTabBarGesture = () => {
+  let gesture: Gesture;
+  let currentTouchedButton: HTMLIonTabButtonElement | null;
+  const tabEffectEl = cloneElement('ion-tab-button');
+  const ionTabBar = document.querySelector('ion-tab-bar') as HTMLElement;
+  const gestureName = 'tab-bar-gesture';
+
+  ionTabBar.addEventListener('pointerdown', () => {
+    clearActivated();
+  });
+
+  const createTabButtonGesture = () => {
+    gesture = createGesture({
+      el: ionTabBar,
+      threshold: 0,
+      gestureName,
+      onStart: (event) => enterTabButtonAnimation(event),
+      onEnd: (event) => leaveTabButtonAnimation(event),
+    });
+    gesture.enable(true);
+  };
+  createTabButtonGesture();
+
+  const clearActivated = (isAfterAddWrite = false) => {
+    if (currentTouchedButton) {
+      tabEffectEl.style.display = 'none';
+      tabEffectEl.innerHTML = '';
+      tabEffectEl.style.top = 'auto';
+      tabEffectEl.style.top = 'left';
+      tabEffectEl.style.transform = 'none';
+
+      currentTouchedButton!.classList.remove('ion-activated');
+      if (isAfterAddWrite) {
+        currentTouchedButton.click();
+      }
+
+      /**
+       * もしこの処理がafterAddWrite以外で走る場合、正常に終了していない
+       */
+      if (!isAfterAddWrite) {
+        gesture.destroy();
+        createTabButtonGesture();
+      }
+      currentTouchedButton = null;
+    }
+  };
+
+  const enterTabButtonAnimation = (detail: GestureDetail) => {
+    currentTouchedButton = (detail.event.target as HTMLElement).closest('ion-tab-button');
+    const tabSelectedActual = document.querySelector('ion-tab-button.tab-selected');
+    if (tabSelectedActual === null || currentTouchedButton === null) {
+      return;
+    }
+
+    const tabButtonAnimation = createAnimation();
+    tabButtonAnimation.addElement(tabEffectEl);
+    tabButtonAnimation
+      .delay(70)
+      .beforeStyles({
+        width: `${tabSelectedActual.clientWidth}px`,
+        height: `${tabSelectedActual.clientHeight}px`,
+        display: 'block',
+      })
+      .beforeAddWrite(() => {
+        tabSelectedActual.childNodes.forEach((node) => {
+          tabEffectEl.appendChild(node.cloneNode(true));
+        });
+        currentTouchedButton!.classList.add('ion-activated');
+      })
+      .duration(70)
+      .keyframes([
+        {
+          transform: 'scale(1.1)',
+          left: `${tabSelectedActual.getBoundingClientRect().left}px`,
+          top: `${tabSelectedActual.getBoundingClientRect().top}px`,
+          opacity: 1,
+        },
+        {
+          transform: 'scale(1.42)',
+          left: `${currentTouchedButton!.getBoundingClientRect().left}px`,
+          top: `${currentTouchedButton!.getBoundingClientRect().top}px`,
+          opacity: 1,
+        },
+      ]);
+
+    tabButtonAnimation.play();
+  };
+
+  const leaveTabButtonAnimation = (detail: GestureDetail) => {
+    const tabSelectedActual = document.querySelector('ion-tab-button.tab-selected');
+    if (tabSelectedActual === null || currentTouchedButton === null) {
+      return;
+    }
+
+    const tabButtonAnimation = createAnimation();
+    tabButtonAnimation.addElement(tabEffectEl);
+    tabButtonAnimation
+      .afterAddWrite(() => clearActivated(true))
+      .duration(50)
+      .keyframes([
+        {
+          transform: 'scale(1.42)',
+          left: `${currentTouchedButton!.getBoundingClientRect().left}px`,
+          top: `${currentTouchedButton!.getBoundingClientRect().top}px`,
+          opacity: 1,
+        },
+        {
+          transform: 'scale(1.1)',
+          left: `${currentTouchedButton!.getBoundingClientRect().left}px`,
+          top: `${currentTouchedButton!.getBoundingClientRect().top}px`,
+          opacity: 0,
+        },
+      ]);
+
+    tabButtonAnimation.play();
+  };
+};
